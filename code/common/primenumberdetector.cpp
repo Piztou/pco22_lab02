6b9be46a94e60b5aba9cd4f090914543346bf47f
Yann Merk <yannmerk@gmail.com>
2022-10-06 14:21:44 +0200

    Added single threaded isPrime

--- code/common/primenumberdetector.cpp
+++ code/common/primenumberdetector.cpp
@@ -1,10 +1,18 @@
-// Author(s) : Prénom Nom, Prénom Nom
+// Author(s) : Adrian Barreira Romero, Yann Merk
 
 #include "primenumberdetector.h"
 
 bool PrimeNumberDetector::isPrime(uint64_t number) {
-    // TODO
-    return false;
+    // Corner case
+    if (number <= 1)
+        return false;
+
+    // Check from 2 to n-1
+    for (int i = 2; i < sqrt(number); i++)
+        if (number % i == 0)
+            return false;
+
+    return true;
 }
 
 PrimeNumberDetectorMultiThread::PrimeNumberDetectorMultiThread(size_t nbThreads) {
